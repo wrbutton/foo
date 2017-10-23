32cff79885683492635e72e676ea5330683026a1
wrbutton <willis.readbutton@gmail.com>
2017-10-23 14:13:03 -0400

    Add files via upload

---
+++ csv_subset.py
@@ -0,0 +1,182 @@
+
+import pt, gcsv, os, csv, sys
+import gcsv_2
+import pandas as pd
+
+
+def split_rename_csvs(path='dflt', mapfile='dflt'):
+    """ updated October 2017, relies upon other gcsv functions
+    but all in one batch and split them apart into individual csv files which can
+    be run as RPTWLS. Requires input excel file of form below, can list many files.
+    
+    assumes to be in target directory named 'mapping.xlsx'
+    Can use same Det Well as Destination well if simply want to split out
+    
+    CSV Name	            Det Well	Destination
+    ITP549_DP52_RPTWLS.csv	E1	                C11
+    ITP549_DP52_RPTWLS.csv	F1	                D11
+    ITP553_DP52_RPTWLS.csv	I1	                C11
+    ITP553_DP52_RPTWLS.csv	J1	                D11
+    """
+    if path is 'dflt':
+        path = '/Users/WRB/Desktop/splitcsv/'
+    if mapfile is 'dflt':
+        mapfile = os.path.join(path, 'mapping.xlsx')
+
+    try:
+        # load the plate/well remapping file
+        m = pd.read_excel(mapfile)
+        dest_csvs = m['CSV Name'].unique()
+        src_csvs = pt.get_flist(path, '.csv')
+    except:
+        sys.exit(" dflt path is '/Users/WRB/Desktop/splitcsv/' and assumes 1 csv and mapping.xlsx")
+
+    try:
+        src_52_csv = [x for x in src_csvs if 'DP52' in x and 'RPTWLS.csv' not in x][0]
+    except:
+        print('no d52 source csv')
+    try:
+        src_53_csv = [x for x in src_csvs if 'DP53' in x and 'RPTWLS.csv' not in x][0]
+    except:
+        print('no d53 source csv')
+
+    # loop through destination csvs pulled from mapping file
+    for csv_file in dest_csvs:
+        # get subset of the plate remapping dataframe for the same plate
+        subm = m[m['CSV Name']==csv_file]
+        outpath = os.path.join(path, csv_file)
+        # convert dataframe of excel remapping into an easy dictionary
+        dest = list(subm['Destination'].values)
+        src = gcsv_2.get_2char_ids(list(subm['Det Well'].values))
+        dest = gcsv_2.get_2char_ids(list(subm['Destination'].values))
+        mydict = dict(zip(src, dest))
+        # then call on the split_csv command in gcsv module
+        if 'DP52' in csv_file:
+            print(os.path.split(src_52_csv)[-1] + ' => ' + csv_file)
+            gcsv_2.split_out_csv(src_52_csv, mydict, outpath)
+        elif 'DP53' in csv_file:
+            print(os.path.split(src_53_csv)[-1] + ' => ' + csv_file)
+            gcsv_2.split_out_csv(src_53_csv, mydict, outpath)
+        else:
+            print('no match')
+
+
+def adj_rptwls_to_plates(platepath='dflt', subpath='dflt'):
+    """ batch adjust individual repeat wells files, assuming single batch per plate
+    derives adjustment vector for each RPTWLS compared to full plate for bead set and runs
+    if there are plates that have multiple batches, the plate map for those should be placed
+    in the plates folder, and if present adj_vectors will be made on a per-batch basis """
+
+    if platepath is 'dflt':
+        platepath = '/Users/WRB/Desktop/plates/'
+    if subpath is 'dflt':
+        subpath = '/Users/WRB/Desktop/rpts/'
+
+    pflist = pt.get_flist(platepath, '.csv')
+    sflist = pt.get_flist(subpath, '.csv')
+    sflist = [x for x in sflist if 'adjusted' not in x]
+    maplist = [x for x in pt.get_flist(platepath, '.xlsx')]
+
+    pshn, sshn = {}, {}
+
+    for p in pflist:
+        sh = '_'.join(os.path.split(p)[1].split('_')[:2])
+        pshn[sh] = p
+    for s in sflist:
+        sh = '_'.join(os.path.split(s)[1].split('_')[:2])
+        sshn[sh] = s
+
+    for s in set(sshn.keys()):
+        print(s)
+        platefile = pshn[s]
+        subfile = sshn[s]
+        try:
+            mapfile = [x for x in maplist if s.split('_')[0] in x][0]
+        except:
+            mapfile = None
+
+        if mapfile:
+            print('adjusting ' + s + ' by batch with map')
+            m = pd.read_excel(mapfile)
+            batches = m['batch'].dropna().unique()
+            badjvect = {}
+            dp = gcsv.open_as_gct(platefile)
+            ds = gcsv.open_as_gct(subfile)
+            subwells = ds.columns.values
+            pwells = [x for x in dp.columns.values if x not in subwells]
+            for b in batches:
+                wids = m[m['batch'] == b]['well'].values
+                pids = [x for x in wids if x in pwells]
+                sids = [x for x in wids if x in subwells]
+                pbmed = dp[pids].median(axis=1)
+                sbmed = ds[sids].median(axis=1)
+                badjvect[b] = round(pbmed - sbmed)
+            gcsv_2.adj_csv(subfile, adj_vect=badjvect, pmap=m)
+        # if no mapfile present, proceed with single batch adj_vect
+        else:
+            print('adjusting ' + s)
+            d = gcsv.open_as_gct(platefile)
+            pmed = d.median(axis=1)
+            d = gcsv.open_as_gct(subfile)
+            submed = d.median(axis=1)
+            diff_vect = round(pmed - submed)
+            gcsv_2.adj_csv(subfile, adj_vect=diff_vect)
+
+
+def adj_fillin_wells(path='dflt', top=True, bot=True):
+    """ adjust full csv files for the traditional Turbocapture problem areas, default top and bottom
+     if a given plate map file is present in the folder for a given plate, then individual batch
+     adj_vectors are prepared and passed through"""
+
+    if path is 'dflt':
+        path = '/Users/WRB/Desktop/plates/'
+
+    try:
+        pflist = pt.get_flist(path, '.csv')
+        maplist = pt.get_flist(path, '.xlsx')
+        pflist = [x for x in pflist if 'adjusted' not in x]
+    except:
+        sys.exit("dflt path is '/Users/WRB/Desktop/plates/' and top/bot = True")
+
+    mywells = []
+    if top is True:
+        mywells.extend(pt.txt2list('/Users/wrb/Dropbox/bin/python/topwells.txt', single=True))
+    if bot is True:
+        mywells.extend(pt.txt2list('/Users/wrb/Dropbox/bin/python/botwells.txt', single=True))
+
+    mshn = {}
+    for m in maplist:
+        sh = pt.get_shn(m).strip('.xlsx')
+        print(sh)
+        mshn[sh] = m
+    print(maplist)
+
+    for platefile in pflist:
+        plate_name = pt.get_shn(platefile)
+        d = gcsv.open_as_gct(platefile)
+        # if excel map file is in directory, cycle through to get batch adj vectors
+        if plate_name in mshn.keys():
+            print('adjusting ' + plate_name + ' by batch with map')
+            m = pd.read_excel(mshn[plate_name])
+            batches = m['batch'].dropna().unique()
+            badjvect = {}
+            pwells = [x for x in d.columns.values if x not in mywells]
+            for b in batches:
+                wids = m[m['batch'] == b]['well'].values
+                pids = [x for x in wids if x in pwells]
+                sids = [x for x in wids if x in mywells]
+                pbmed = d[pids].median(axis=1)
+                sbmed = d[sids].median(axis=1)
+                badjvect[b] = round(pbmed - sbmed)
+            gcsv_2.adj_csv(platefile, mywells=mywells, adj_vect=badjvect, pmap=m)
+        # otherwise just proceed with single batch adjustment
+        else:
+            print('adjusting ' + plate_name)
+            pmed = d.drop(mywells, axis=1).median(axis=1)
+            sub = d[mywells]
+            submed = sub.median(axis=1)
+            diff_vect = round(pmed - submed)
+            gcsv_2.adj_csv(platefile, mywells=mywells, adj_vect=diff_vect)
+
+
+
---
+++ gcsv.py
@@ -0,0 +1,443 @@
+#!/usr/bin/python
+"""
+12/13/16 - branched from gct into a csv class
+                     will extract dataframe from the Median section of csv file
+                     can also load a panel, and combine multiple plates for median/avg
+                     saves a file by mimicing the structure of another csv file to
+                     ensure pipeline compatibility
+01/19/17 - added batch_adjust command to edit many csvs at once, and the
+                     summarize_csvs to get basic metrics before processing
+
+"""
+
+import pandas as pd
+import numpy as np
+import collections as cll
+import os, csv, gct, sys, pt, math
+import pickle
+
+
+def cmbine_save(path, ctype):
+    combined = get_combined(path, ctype)
+    for filep in os.listdir(path):
+        if filep.endswith('.csv'):
+            c0 = Gcsv(filep)
+            break
+    csvf = os.path.join(path, c0.file)
+    fname = '{}_{}.csv'.format(c0.shortname, ctype)
+    output = os.path.join(path, fname)
+    save_csv(csvf, combined, output)
+
+
+def get_combined(path, ctype):
+    # quick method to get median or mean of a panel
+    panel = load_cpanel(path, pan=True)
+    if ctype == 'median':
+        combined = panel.median(axis=0)
+        print('generating media of panel')
+    elif ctype == 'mean':
+        combined = panel.mean(axis=0)
+        print('generating mean of panel')
+    else:
+        print('please enter valid combine type, returning blank')
+        combined = []
+    return combined
+
+
+def load_cpanel(path, pan=False):
+    # given folder path gets gct info and builds dframes for files
+    # either merges into panel, or returns 2 dicts of gcts and dframes
+    f_list = gct.get_flist(path, '.csv')
+    gd, dfd = {}, {}
+    for i, file in enumerate(f_list):
+        gn, dfn = 'c' + str(i), 'df' + str(i)
+        gd[gn] = Gcsv(file)
+        dfd[dfn] = gd[gn].build_dframe()
+    if pan == True:
+        panel = pd.Panel({i: df for i, df in enumerate(dfd.values())})
+        return panel
+    elif pan == False:
+        return gd, dfd
+
+
+def summarize_csvs(path):
+    results = cll.defaultdict(dict)
+    f_list = gct.get_flist(path, '.csv')
+    for file in f_list:
+        c = Gcsv(file)
+        d = c.build_dframe()
+        results[c.shortname]['plate-L10'] = d['Analyte 10'].mean(axis=0)
+        results[c.shortname]['Pos-L10'] = d.ix['B1']['Analyte 10']
+        results[c.shortname]['Ref-L10'] = d.ix[['A2','B2']]['Analyte 10'].mean()
+        results[c.shortname]['plate-L1'] = d['Analyte 1'].mean(axis=0)
+    res = pd.DataFrame(results)
+    res = res.T
+    outpath = os.path.join(path, 'csv_summary.txt')
+    res.to_csv(outpath, sep='\t', float_format='%.0f')
+
+
+# method to save a dafaframe object with gct file structure and headers
+def save_csv(csvf, df, outpath):
+    df = df.fillna('NaN')
+    with open(csvf, 'rU') as in_file, open(outpath, 'w', newline='') as out_file:
+        linereader = csv.reader(in_file, delimiter=',', quotechar='"')
+        csvwriter = csv.writer(out_file, delimiter=',', quotechar='"',
+                                                         quoting=csv.QUOTE_ALL)
+        pd.options.display.float_format = '{i}'.format
+        f = 0
+        for line in linereader:
+        # read through file until correct data section, writing uninteristing lines
+        # to the corresponding edited file as-is
+            if 'Data Type:' and 'Median' in line:
+                csvwriter.writerow(line)
+                # flag to read one more line and then change modes
+                f = 1
+            elif f == 1:
+                csvwriter.writerow(line)
+                break
+            else:
+                csvwriter.writerow(line)
+        # read lines until blank checking wells and for matched ids from file
+        for i in range(0,384):
+            line = next(linereader)
+            #dlist = ['{:.2f}'.format(x) if isnum(x) else 'NaN' for x in df.ix[i]]
+            try:
+                dlist = [x if isnum(x) else 'NaN' for x in df.ix[i]]
+                line[2:502] = dlist
+                csvwriter.writerow(line)
+            except IndexError:
+                csvwriter.writerow(line)
+                break
+        # then just copy over the rest of the lines
+        for line in linereader:
+            csvwriter.writerow(line)
+
+
+def check_11499(csvf):
+    with open(csvf, 'rU') as in_file:
+        linereader = csv.reader(in_file, delimiter=',', quotechar='"')
+        pd.options.display.float_format = '{i}'.format
+        for line in linereader:
+            if line == ['DataType:', 'Count']:
+                break
+        line = next(linereader)
+        line = next(linereader)
+        c11 = line[12]
+        c499 = line[500]
+        fn = os.path.split(csvf)[-1]
+        shn = '_'.join(fn.split('_')[0:2])
+        print(fn, '\t', '11 count:', '{:4}'.format(c11), '\t', '499 count:', '{:4}'.format(c499))
+
+
+def split_out_csv(csvf, d, outpath):
+    """ takes in a larger csvfile to pull from, a dictionary 'd' mapping original to destination well,
+    an outpath for the file. requires initial csv file, a dictionary of source: destination well mapping
+    !! but is that dictionary in 2 or 3 character well format? !!
+    counts are automatically scaled up 1.5x (but 499 and 11 left alone) """
+    with open(csvf, 'rU') as in_file, open(outpath, 'w', newline='') as out_file:
+        # set up csv file line writers and readers
+        linereader = csv.reader(in_file, delimiter=',', quotechar='"')
+        csvwriter = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        pd.options.display.float_format = '{i}'.format
+        # initialize flags and variables
+        q, f, csv_section = 0, 0, 0
+        sample_names = []
+        # define the csv file sections of interest
+        sections = ['Median', 'Net MFI', 'Count', 'Avg Net MFI', 'Dilution Factor']
+        # read through csv file until correct data section, writing uninteresting lines as is
+        for line in linereader:
+            # check for csv file sections of interest
+            if any(sec in line for sec in sections) and 'DataType:' in line:
+                # write that last line
+                csvwriter.writerow(line)
+                # flag to change file write mode to active data replacement
+                f = 1
+            elif f == 1:
+                # write the header row just read
+                csvwriter.writerow(line)
+                # accept back the linereader object to continue moving through file
+                # for line in linereader:
+                for i in range(0, 384):
+                    line = next(linereader)
+                    # despite what pycharm says, needs to be line == [], NOT line is []
+                    if line == []:
+                        csvwriter.writerow(line)
+                        break
+                    # catch for the Avg Net MFI section, referencing the 'unknown2' names
+                    if csv_section == 3:
+                        if line[0] in sample_names:
+                            csvwriter.writerow(line)
+                            continue
+                        else:
+                            continue
+                    # get the luminex well id
+                    wid = line[0].split(',')[1].rstrip(')')
+                    if wid in d.keys():
+                        hdr = line[0].split(',')[0]
+                        print(wid, '=>', d[wid])
+                        nh = hdr + ',' + d[wid] + ')'
+                        line[0] = nh
+                        if csv_section == 2:
+                            # store the 'unknown2' names to strip out unneeded for AVG MFI
+                            sample_names.append(line[1])
+                            # store 11 and 499 counts as is
+                            c11, c499 = line[12], line[500]
+                            # multiply counts to incorporate rptwls
+                            line[2:502] = [int(x) * 1.5 for x in line[2:502]]
+                            # fill back in original counts
+                            line[12], line[500] = c11, c499
+                        csvwriter.writerow(line)
+                # reset flag to be non-replace mode
+                f = 0
+                # increment the section header
+                csv_section += 1
+            else:
+                csvwriter.writerow(line)
+                continue
+
+
+def adj_csv(csvf, outpath='dflt', mywells='all', pmap=None, adj_vect=None, separate=False, fixcounts=False, scalefactor=1):
+    """ adjusts a csv file (whole or from list of wells) adjusting well values either by applying an adj_vector
+    to each well, or by a scalefactor to each well. if a platemap is provided, then batch adjustment vectors will
+    be applied. if separate is True only the wells in mywells list are written to file, otherwise only those are
+    edited but all wells are copied over to new file. fixcounts ajdusts counts by 1.5, but if the file has been
+    split out from a consolidated csv into sub RPTWLS files the count adjustment has been applied then """
+    if outpath is 'dflt':
+        outpath = csvf.strip('.csv') + '_adjusted.csv'
+    if mywells is 'all':
+        mywells = get_2char_ids(pt.get_awells())
+    else:
+        mywells = get_2char_ids(mywells)
+    if adj_vect is None:
+        adj_vect = [0] * 500
+
+    print('adjusting ' + str(mywells))
+
+    with open(csvf, 'rU') as in_file, open(outpath, 'w', newline='') as out_file:
+        linereader = csv.reader(in_file, delimiter=',', quotechar='"')
+        csvwriter = csv.writer(out_file, delimiter=',', quotechar='"',
+                               quoting=csv.QUOTE_ALL)
+        pd.options.display.float_format = '{i}'.format
+        # initialize flags and variables
+        flag, csv_section = 0, 0
+        # define the csv file sections of interest
+        hdr = ['Median', 'Net MFI', 'Count']
+        # read through csv file until correct data section, writing uninteristing lines
+        # to the corresponding edited file as-is
+        for line in linereader:
+            # check for csv file sections of interest
+            if any(h in line for h in hdr):
+                if 'DataType:' in line:
+                    # write that last line
+                    csvwriter.writerow(line)
+                    # flag to change file write mode to active data replacement
+                    flag = 1
+            # pick up the next line and edit if it's a well of interest
+            elif flag == 1:
+                # write the header row just read, and loop through lines up to a full plate
+                csvwriter.writerow(line)
+                for i in range(0, 384):
+                    # increment and get the next line
+                    line = next(linereader)
+                    # if a blank line (reached end of section) exit out
+                    if line == []:
+                        csvwriter.writerow(line)
+                        break
+                    # within MFI and Net MFI sections of csv
+                    if csv_section < 2:
+                        # want to only pull wells of interest to edit
+                        well = line[0].split(',')[1].rstrip(')')
+                        if well in mywells:
+                            vals = [round(float(x) * scalefactor) if x != 'NaN' else 0 for x in line[2:502]]
+                            # if a plate map is provided, applies the per-batch adj-vector (which should be dict)
+                            # otherwise applies a global adj_vector as a list to each well
+                            if pmap is not None:
+                                b = pmap[pmap['well'] == well]['batch'].values[0]
+                                z = zip(vals, list(adj_vect[b]))
+                            else:
+                                z = zip(vals, list(adj_vect))
+                            # combines scaled new with adj values using weights above
+                            newvals = [abs(x[0] + x[1]) for x in z]
+                            # insert new values into the line to be written
+                            line[2:502] = newvals
+                            csvwriter.writerow(line)
+                            continue
+                        elif well not in mywells:
+                            if separate is False:
+                                csvwriter.writerow(line)
+                    # within the Count section of the csv, scale up if fixcounts is True
+                    if csv_section == 2:
+                        well = line[0].split(',')[1].rstrip(')')
+                        if well in mywells:
+                            if fixcounts is True:
+                                # scale up counts, but store 11 and 499 counts as is
+                                c11, c499 = line[12], line[500]
+                                line[2:502] = [float(x) * 1.5 for x in line[2:502]]
+                                line[12], line[500] = c11, c499
+                            elif fixcounts is False:
+                                csvwriter.writerow(line)
+                        elif well not in mywells:
+                            if separate is False:
+                                csvwriter.writerow(line)
+                # reset flag to be non-replace mode
+                flag = 0
+                # increment the section header
+                csv_section += 1
+                continue
+            else:
+                csvwriter.writerow(line)
+
+
+
+def get_2char_ids(wells):
+    """ go from 3 char ids to the dumb luminex format """
+    new_wells = []
+    for w in wells:
+        if len(w) == 3:
+            if w[1] == '0':
+                new_wells.append(w[0] + w[2])
+            else:
+                new_wells.append(w)
+        elif len(w) == 2:
+            new_wells.append(w)
+    return new_wells
+
+
+def get_3char_ids(wells):
+    new_wells = []
+    # wells = [w.split(',')[1].strip(')') for w in wells]
+    for w in wells:
+        if len(w) < 3:
+            new_wells.append(w[0] + '0' + w[1])
+        elif len(w) == 3:
+            new_wells.append(w)
+    return new_wells
+
+
+def bulk_open_as_gct(path, drop_inv=False):
+    # no current support for repeat wells files
+    flist = pt.get_flist(path, '.csv')
+    pdict = cll.defaultdict(list)
+    for f in flist:
+        shn = pt.get_shn(f)
+        pdict[shn].append(f)
+    for k, v in pdict.items():
+        bsets = [x.split('_')[1] for x in v]
+        if 'DP52' in bsets and 'DP53' in bsets and len(bsets) < 3:
+            print(k, ' ok')
+            df1 = open_as_gct(v[0], log=True)
+            df2 = open_as_gct(v[1], log=True)
+            df = pd.concat([df1, df2], axis=0)
+            df.sort_index(inplace=True)
+            if drop_inv is True:
+                df = df[~df.index.str.contains('INV')]
+            else:
+                print('watch out, invariant genes included')
+            outpath = os.path.join(path, k + '_pkexp.gct')
+            gct.save_simplegct(df, outpath)
+        else:
+            print('error with: ', k)
+
+
+def open_as_gct(file, log=False):
+    csv = Gcsv(file)
+    df = csv.transform_to_gct()
+    if log is True:
+        df = df.applymap(lambda x: '{:.3f}'.format(math.log(x, 2)))
+    return df
+
+
+def isnum(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+class Gcsv(object):
+    # an object ossociated with extracting info from written gct files
+
+    def __init__(self, infile):
+        # pass the file argument as the filepath reference
+        self.file = infile
+        self.shortname = '_'.join(infile.split('/')[-1].split('_')[:2])
+
+    def get_headers(self):
+        # open file and extract the gct header information, storing dictionary values
+        wells, hdrcl = [],[]
+        with open(self.file, 'rU') as file:
+            linereader = csv.reader(file, delimiter=',', quotechar='"')
+            for i, line in enumerate(linereader):
+                if 'Data Type:' and 'Median' in line:
+                     self.skrows = i + 1
+                     break
+            for i, line in enumerate(linereader):
+                if i >= 385:
+                    self.samples = i
+                    break
+                if i == 0:
+                    continue
+                else:
+                    if line == []:
+                        self.samples = i
+                        break
+                    hdrcl.append(line[:2])
+                    try:
+                        well = line[0].split(',')[1].rstrip(')')
+                        wells.append(well)
+                    except IndexError:
+                        print('')
+                        print(line)
+                        print('')
+                        print('oops, no support yet for non-full csv files')
+                        sys.exit()
+            self.wells = wells
+            self.hdrcl = hdrcl
+            #print(self.wells)
+
+    def null_empty(self, data):
+        # used to replace process control wells with 'nan' so they won't plot
+        for w in self.empty:
+            data.iloc[:,w] = np.nan
+        return data
+
+    def build_dframe(self):
+        # open file and extract the gct header information, storing dictionary values
+        self.get_headers()
+        # skip rows down to data, get first column value as index identifier
+        data = pd.read_csv(self.file, sep=',', quotechar='"', skiprows=self.skrows,
+                           nrows=self.samples-1, usecols=range(2,502))
+        data.shortname = pt.get_shn(self.file)
+        # retitle the index column name from Unnamed:0
+        data = data.set_index([self.wells])
+        # give back the completed dataframe with row + column ids
+        return data
+
+    def transform_to_gct(self):
+        df = self.build_dframe()
+        new_index = get_3char_ids(df.index)
+        df.index = new_index
+        # ref_file = '/Users/WRB/Dropbox/bin/python/analytes_to_probes_dict.p'
+        ref_file = '/Users/WRB/Dropbox/bin/python/analytes_to_probes_dict.p'
+        analyte_dict = pickle.load(open(ref_file, 'rb'))
+        if 'DP52' in self.file:
+            beadset_dict = analyte_dict['dp52']
+        elif 'DP53' in self.file:
+            beadset_dict = analyte_dict['dp53']
+        genes = [col.lstrip('Analyte ') for col in df.columns]
+        genes = [beadset_dict[int(analyte)] for analyte in genes]
+        genes = ['0_' + x if 'INV' in x else x for x in genes]
+        if 'DP52' in self.file:
+            df['Analyte 499'] = 0
+        elif 'DP53' in self.file:
+            df['Analyte 11'] = 0
+        df.columns = genes
+        df.replace('NaN', 0)
+        # del df['NaN']
+        wells = get_3char_ids(df.index)
+        df.index = wells
+        df = df.T
+        return df
+
+
